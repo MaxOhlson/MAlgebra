d3012c9b118256cfc19d4c99edabdf95ee921864
MaxOhlson <144359605+MaxOhlson@users.noreply.github.com>
2023-11-01 17:43:56 +0100

    Add files via upload

---
+++ SANNOLIKHET.py
@@ -0,0 +1,14 @@
+# det finns 1024 sätt att tillordna sanningsvärden till utsagorna P1 - P10
+# Hur många av dessa gör att följande utsaga blir sann?
+# A U B = a or b
+# A snitt B = a and b
+# A v B = a eller b
+# A A B = a och b 
+#Om ((P1 eller P2) och (P3 eller P4) och (P5 eller P6)) Så ((P7 och P8) eller (P9 och P10))
+# Om a och b och c så d eller f
+# detta gäller när hela Vl är sant och hela Hl är sant
+# detta gäller när VL är falsk och HL sant
+# detta gäller när VL är falsk och HL är falskt
+# ovanstående kan också beskrivas som 1024 - när det inte stämmer, dvs VL = sant och HL = falsk
+# Alltså, när är VL = sant ?
+# och när är HL = falsk? Ursprungssiffran minus när detta intrÄFFAR BORDE VARA SVARET. 
